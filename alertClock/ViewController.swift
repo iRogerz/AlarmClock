526b7450f3288f925017b128e80478fc66a95299
roger <rogertseng1025@gmail.com>
2022-04-15 11:57:59 +0800

    Initial Commit

---
+++ alertClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alertClock
+//
+//  Created by 曾子庭 on 2022/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
