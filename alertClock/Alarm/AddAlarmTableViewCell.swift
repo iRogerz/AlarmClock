76556ea91b86c324c4a09def0f431a1d4f6eb8f6
Roger Tseng <rogertseng1025@gmail.com>
2022-04-19 18:22:00 +0800

    Feat:AddAlarmTableView appearance

--- alertClock/Alarm/AddAlarmTableViewCell.swift
+++ alertClock/Alarm/AddAlarmTableViewCell.swift
@@ -6,18 +6,72 @@
 //
 
 import UIKit
+import SnapKit
 
 class AddAlarmTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        //顯示 ">" 符號
+        self.accessoryView = detailImageView
+//        self.accessoryView = UISwitch(frame: .zero)
+        
+        setupUI()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    //MARK: - setupUI
+    let titleLabel:UILabel = {
+        let myLabel = UILabel()
+        myLabel.textColor = .white
+        return myLabel
+    }()
+    
+    let contentLabel:UILabel = {
+        let myLabel = UILabel()
+        myLabel.textColor = .lightGray
+        return myLabel
+    }()
+//    let tableSwitch:UISwitch = {
+//        let mySwitch = UISwitch()
+//        return mySwitch
+//    }()
+    
+    let detailImageView: UIImageView = {
+            // 設定這個 imageView 的圖案為 "chevron.right"
+            let imageView = UIImageView(image: UIImage(systemName: "chevron.right"))
+            // 設定這個 imageView 的顏色為灰色
+            imageView.tintColor = .lightGray
+            return imageView
+        }()
+    
+    static let identifier = "AddAlarmTableViewCell"
+    
+    func setupUI(){
+        self.addSubview(titleLabel)
+        self.addSubview(contentLabel)
+//        self.addSubview(tableSwitch)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self)
+            make.leading.equalTo(self).offset(14)
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self)
+            make.trailing.equalTo(self).offset(-50)
+        }
+//        tableSwitch.snp.makeConstraints { make in
+//            make.top.bottom.equalTo(self)
+//            make.trailing.equalTo(self).offset(-50)
+//        }
+    }
+    
+    
+    
 }
--- alertClock/Alarm/AddAlarmViewController.swift
+++ alertClock/Alarm/AddAlarmViewController.swift
@@ -10,13 +10,19 @@ import SnapKit
 
 class AddAlarmViewController: UIViewController {
     
-    //MARK: - UI
+    //MARK: - UI& Variable
     let pickerView: UIPickerView = {
-       let myPicker = UIPickerView()
+        let myPicker = UIPickerView()
+        myPicker.setValue(UIColor.white, forKey: "textColor")
         return myPicker
     }()
     let tableView:UITableView = {
-       let myTable = UITableView()
+        let myTable = UITableView()
+        myTable.layer.cornerRadius = 10
+        
+        //註冊addalarmtableviewcell
+        myTable.register(AddAlarmTableViewCell.self, forCellReuseIdentifier: AddAlarmTableViewCell.identifier)
+        
         return myTable
     }()
     
@@ -33,21 +39,30 @@ class AddAlarmViewController: UIViewController {
         times.append(tempHR)
         times.append(tempMin)
     }
-   
+    
+    let addAlarmTitle = ["Repeat", "Lebal", "Sound", "Snooze"]
+    var addAlarmContent = ["Never", "Alarm", "Rader"]
+    
+    
+    //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
-        pickerView.dataSource = self
-        pickerView.delegate = self
-        setupNavigation()
-        setupUI()
+        view.backgroundColor = #colorLiteral(red: 0.1669761635, green: 0.1807887411, blue: 0.2008549854, alpha: 1)
+        
         setupTimes()
-        pickerView.selectRow(0, inComponent: 0, animated: true)
-        pickerView.selectRow(1, inComponent: 0, animated: true)
+        setupUI()
+        setupNavigation()
+        //        var hr = pickerView.selectRow(0, inComponent: 0, animated: true)
+        //        var min = pickerView.selectRow(0, inComponent: 1, animated: true)
     }
     
     //MARK: - setupUI
     func setupUI(){
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
         self.view.addSubview(pickerView)
         self.view.addSubview(tableView)
         
@@ -57,8 +72,9 @@ class AddAlarmViewController: UIViewController {
         }
         
         tableView.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(view)
-            make.top.equalTo(pickerView.snp.bottom).offset(50)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(18)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(pickerView.snp.bottom).offset(42)
             make.height.equalTo(200)
         }
     }
@@ -69,7 +85,7 @@ class AddAlarmViewController: UIViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButton))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButton))
         
-//        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.1669761635, green: 0.1807887411, blue: 0.2008549854, alpha: 1)
+        //        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.1669761635, green: 0.1807887411, blue: 0.2008549854, alpha: 1)
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor:UIColor.white]
         navigationItem.rightBarButtonItem?.tintColor = .orange
         navigationItem.leftBarButtonItem?.tintColor = .orange
@@ -82,8 +98,15 @@ class AddAlarmViewController: UIViewController {
     @objc func saveButton(){
         
     }
+    @objc func switchChanged(_ sender : UISwitch!){
+
+          print("table row switch Changed \(sender.tag)")
+          print("The switch is \(sender.isOn ? "ON" : "OFF")")
+    }
+    
 }
 
+//MARK: - UIPicker
 extension AddAlarmViewController: UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return times.count
@@ -92,11 +115,59 @@ extension AddAlarmViewController: UIPickerViewDataSource{
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return times[component].count
     }
+    //    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+    //        let attr = NSAttributedString(string: String, attributes: [NSForegroundColorAttributeName:UIColor]?)
+    //    }
 }
 
 extension AddAlarmViewController:UIPickerViewDelegate{
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         let title = times[component][row]
         return title
+    }
+    
+    //讓兩個間距變窄一點
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 66
+    }
+    
+}
+
+
+//MARK: - UITable
+extension AddAlarmViewController:UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return addAlarmTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        switch indexPath.row{
+        case 3:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: AddAlarmTableViewCell.identifier, for: indexPath) as? AddAlarmTableViewCell else{ return UITableViewCell() }
+            cell.titleLabel.text = addAlarmTitle[indexPath.row]
+            
+            //建立UISwitch 到 tableView
+            let switchView = UISwitch(frame: .zero)
+            switchView.setOn(false, animated: true)
+            switchView.tag = indexPath.row // for detect which row switch Changed
+            switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+            cell.accessoryView = switchView
+            return cell
+        default:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: AddAlarmTableViewCell.identifier, for: indexPath) as? AddAlarmTableViewCell else{ return UITableViewCell() }
+            cell.titleLabel.text = addAlarmTitle[indexPath.row]
+            cell.contentLabel.text = addAlarmContent[indexPath.row]
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    //設tableView背景
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = .darkGray
     }
 }
