c4612e4d2f4bea7849c5120ed50c31d60d8776c2
Roger Tseng <rogertseng1025@gmail.com>
2022-05-24 17:22:57 +0800

    Feat:update Readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # AlarmClock
+hello, I'm Roger, an IOS Developer
+This project is practicing to make an IOS native alarm-clock.
+
+## Environment
+* Xcode 13.3 +
+* Snapkit package
+
+## Features structure
+* MVC structure
+* Refactor data model and userDefault
+* Complete push alarm notification
+* Lot of enum used
+* Delegate pass value
+
+## Demo
+
+![](https://imgur.com/sxhsHnZ)
+
--- alertClock/Helper/Notification.swift
+++ alertClock/Helper/Notification.swift
@@ -13,7 +13,7 @@ import AVFoundation
 class UserNotification{
     var contentHandler: ((UNNotificationContent) -> Void)?
     
-    static func addNotificationRequest(title: String, subTitle: String, id: String) {
+    static func addNotificationRequest(alarm: AlarmInfo) {
         
         let current = UNUserNotificationCenter.current()
         //        current.removeAllPendingNotificationRequests()
@@ -24,20 +24,20 @@ class UserNotification{
         content.sound = UNNotificationSound(named: UNNotificationSoundName("123.wav"))
         //        content.sound = UNNotificationSound.default
         
-        //        let calendar = Calendar.current
-        //        let hour = calendar.component(.hour, from: alarm.date)
-        //        let minute = calendar.component(.minute, from: alarm.date)
-        //        var dateComponents = DateComponents()
-        //        dateComponents.hour = hour
-        //        dateComponents.minute = minute
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: alarm.date)
+        let minute = calendar.component(.minute, from: alarm.date)
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = minute
         
         //測試鬧鐘用的
-        let date = Date().addingTimeInterval(7)
-        let dateComponents = Calendar.current.dateComponents([.hour, .minute, .second], from: date)
+        //        let date = Date().addingTimeInterval(7)
+        //        let dateComponents = Calendar.current.dateComponents([.hour, .minute, .second], from: date)
         
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: alarm.id.uuidString, content: content, trigger: trigger)
         current.add(request) { error in
             if(error == nil){
                 print("successfully")
@@ -47,9 +47,9 @@ class UserNotification{
         }
     }
     
-    static func addNotificationRequest(alarm: AlarmInfo){
-        addNotificationRequest(title: alarm.note, subTitle: "", id: alarm.id.uuidString)
-    }
+//    static func addNotificationRequest(alarm: AlarmInfo){
+//        addNotificationRequest(title: alarm.note, subTitle: "", id: alarm.id.uuidString)
+//    }
 }
 
 
